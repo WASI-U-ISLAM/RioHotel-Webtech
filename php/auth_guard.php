9d04648f96a523fe74d41b053708c535b45059cd
Shams Sadi <shams432@gmail.com>
2025-09-15 16:07:17 +0600

    mod

--- php/auth_guard.php
+++ php/auth_guard.php
@@ -4,10 +4,12 @@ if (!isset($_SESSION['user_id'], $_SESSION['role'])) {
     header('Location: ../view/login.html?error=auth');
     exit();
 }
+
 function require_role(array $roles){
     if (!in_array($_SESSION['role'], $roles, true)) {
         header('Location: ../view/login.html?error=denied');
         exit() ;
     }
+
 }
 ?>
